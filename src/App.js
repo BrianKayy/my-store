9e0711d92216729d4c45774f84f07dc65dcd0dfd
BrianKayy <Briankasumba40@gmail.com>
2023-06-13 22:31:03 +0400

    mycommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import data from  "./Components/Back/Data/Data";
+import Header from "./Components/Front/Header/Header";
+import {BrowserRouter as Router } from "react-router-dom";
+import Routes from './Components/Front/Routes/Routes';
+const App = () => {
 
-function App() {
+  const {  productItems } = data;
+  const {cartItems, setCartItems} = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <Header />
+          <Routes productItems= {productItems} cartItems={cartItems} />
+      </Router>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
